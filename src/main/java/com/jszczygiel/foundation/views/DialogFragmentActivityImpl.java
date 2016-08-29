c9d3e23f0e6318b672cfaba2e4573ef71a9db5e4
jszczygiel <sonykuba@gmail.com>
2016-08-29 22:35:08 +0200

    1. Added dialog dismiss on background click

--- src/main/java/com/jszczygiel/foundation/views/DialogFragmentActivityImpl.java
+++ src/main/java/com/jszczygiel/foundation/views/DialogFragmentActivityImpl.java
@@ -1,5 +1,9 @@
 package com.jszczygiel.foundation.views;
 
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+
 import com.jszczygiel.R;
 
 public abstract class DialogFragmentActivityImpl<T extends BaseFragmentImpl> extends SimpleFragmentActivityImpl<T> {
@@ -8,4 +12,16 @@ public abstract class DialogFragmentActivityImpl<T extends BaseFragmentImpl> ext
         return R.layout.activity_dialog;
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View root = findViewById(R.id.root);
+        root.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.finishAfterTransition(DialogFragmentActivityImpl.this);
+            }
+        });
+    }
 }
