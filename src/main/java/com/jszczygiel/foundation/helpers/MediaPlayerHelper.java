d0c6e9262f3550716c75fcc39c18b3eee546251e
jszczygiel <sonykuba@gmail.com>
2016-09-21 11:43:27 +0200

    1. fixes in media player code

--- src/main/java/com/jszczygiel/foundation/helpers/MediaPlayerHelper.java
+++ src/main/java/com/jszczygiel/foundation/helpers/MediaPlayerHelper.java
@@ -11,6 +11,9 @@ import java.io.File;
 import java.io.FileInputStream;
 
 public class MediaPlayerHelper {
+
+    public static final String CONTENT_SETTINGS_SYSTEM_ALARM_ALERT = "content://settings/system/alarm_alert";
+
     private MediaPlayerHelper() {
     }
 
@@ -42,12 +45,15 @@ public class MediaPlayerHelper {
                         fileInfo);
             } catch (Exception ee) {
                 mp.reset();
-                String uri;
-                if (!fileInfo.equals("content://settings/system/alarm_alert")) {
+                String uri = null;
+                if (fileInfo != null && !CONTENT_SETTINGS_SYSTEM_ALARM_ALERT.equals(fileInfo)) {
                     uri = getRingtoneUriFromPath(context, fileInfo);
                     mp.setDataSource(uri);
-                } else {
-                    mp.setDataSource(context, Uri.parse(fileInfo));
+                }
+
+                if (uri == null || CONTENT_SETTINGS_SYSTEM_ALARM_ALERT.equals(fileInfo)) {
+                    mp.setDataSource(context, Uri.parse(CONTENT_SETTINGS_SYSTEM_ALARM_ALERT));
+
                 }
 
             }
@@ -80,16 +86,19 @@ public class MediaPlayerHelper {
         Cursor ringtoneCursor = context.getContentResolver().query(
                 ringtonesUri, null,
                 MediaStore.Audio.Media.DATA + "='" + path + "'", null, null);
-        ringtoneCursor.moveToFirst();
+        if (ringtoneCursor != null && ringtoneCursor.moveToFirst()) {
 
-        long id = ringtoneCursor.getLong(ringtoneCursor
-                .getColumnIndex(MediaStore.Audio.Media._ID));
-        ringtoneCursor.close();
+            long id = ringtoneCursor.getLong(ringtoneCursor
+                    .getColumnIndex(MediaStore.Audio.Media._ID));
+            ringtoneCursor.close();
 
-        if (!ringtonesUri.toString().endsWith(String.valueOf(id))) {
-            return ringtonesUri + "/" + id;
+            if (!ringtonesUri.toString().endsWith(String.valueOf(id))) {
+                return ringtonesUri + "/" + id;
+            }
+            return ringtonesUri.toString();
+
         }
-        return ringtonesUri.toString();
+        return null;
     }
 
     public static String getRingtonePathFromContentUri(Context context,
@@ -97,26 +106,28 @@ public class MediaPlayerHelper {
         String[] proj = {MediaStore.Audio.Media.DATA};
         Cursor ringtoneCursor = context.getContentResolver().query(contentUri,
                 proj, null, null, null);
-        ringtoneCursor.moveToFirst();
+        if (ringtoneCursor != null && ringtoneCursor.moveToFirst()) {
 
-        String path = ringtoneCursor.getString(ringtoneCursor
-                .getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
+            String path = ringtoneCursor.getString(ringtoneCursor
+                    .getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
 
-        ringtoneCursor.close();
-        return path;
+            ringtoneCursor.close();
+            return path;
+        }
+        return null;
     }
 
     public static boolean isPhoneSilent(Context context) {
         AudioManager audioService = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        boolean ringerModeSilent = false, streamSilent=false;
+        boolean ringerModeSilent = false, streamSilent = false;
         if (audioService.getRingerMode() == AudioManager.RINGER_MODE_SILENT) {
-            ringerModeSilent=true;
+            ringerModeSilent = true;
         }
         int current = audioService.getStreamVolume(AudioManager.STREAM_ALARM);
         if (current <= 1) {
-            streamSilent=true;
+            streamSilent = true;
         }
 
-        return streamSilent||ringerModeSilent;
+        return streamSilent || ringerModeSilent;
     }
 }
