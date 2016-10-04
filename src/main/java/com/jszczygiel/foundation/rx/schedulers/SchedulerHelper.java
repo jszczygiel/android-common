2153a84444566e8337a3c6ef345dfa4f7cd7a206
Jakub Szczygiel <jakub.szczygiel@sportlobster.com>
2016-10-04 22:30:41 +0200

    feat: removed database reader scheduler

--- src/main/java/com/jszczygiel/foundation/rx/schedulers/SchedulerHelper.java
+++ src/main/java/com/jszczygiel/foundation/rx/schedulers/SchedulerHelper.java
@@ -7,19 +7,13 @@ import rx.schedulers.Schedulers;
 
 public class SchedulerHelper {
 
-    private static final Scheduler databaseReaderScheduler;
     private static final Scheduler databaseWriterScheduler;
 
     static {
-        databaseReaderScheduler = Schedulers.from(Executors.newFixedThreadPool(3));
         databaseWriterScheduler = Schedulers.from(Executors.newFixedThreadPool(1));
     }
 
     private SchedulerHelper() {
-    }
-
-    public static Scheduler getDatabaseReaderScheduler() {
-        return databaseReaderScheduler;
     }
 
     public static Scheduler getDatabaseWriterScheduler() {
