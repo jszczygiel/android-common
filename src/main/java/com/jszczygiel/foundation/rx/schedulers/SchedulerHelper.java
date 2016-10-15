e441be0883360b2b79a9a53d8470344728d2c7f5
Jakub Szczygiel <jszczygiel@wroclawstudio.com>
2016-10-15 11:24:46 +0200

    feat: removed app scheduler

--- src/main/java/com/jszczygiel/foundation/rx/schedulers/SchedulerHelper.java
+++ src/main/java/com/jszczygiel/foundation/rx/schedulers/SchedulerHelper.java
@@ -9,19 +9,14 @@ public class SchedulerHelper {
 
     private static final Scheduler databaseWriterScheduler;
 
-    private static final Scheduler applicationScheduler;
 
     static {
         databaseWriterScheduler = Schedulers.from(Executors.newFixedThreadPool(1));
-        applicationScheduler = Schedulers.from(Executors.newFixedThreadPool(4));
     }
 
     private SchedulerHelper() {
     }
 
-    public static Scheduler getApplicationScheduler() {
-        return applicationScheduler;
-    }
 
     public static Scheduler getDatabaseWriterScheduler() {
         return databaseWriterScheduler;
