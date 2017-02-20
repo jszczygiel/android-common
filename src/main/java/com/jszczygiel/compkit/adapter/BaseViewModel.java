97ce8bf1920138ba59df871a20acb61367a44450
Jakub Szczygiel <jszczygiel@wroclawstudio.com>
2017-02-20 20:07:31 +0100

    added hashcode an equals

--- src/main/java/com/jszczygiel/compkit/adapter/BaseViewModel.java
+++ src/main/java/com/jszczygiel/compkit/adapter/BaseViewModel.java
@@ -2,6 +2,9 @@ package com.jszczygiel.compkit.adapter;
 
 import android.os.Parcelable;
 
+import java.util.Arrays;
+
+
 public abstract class BaseViewModel implements Parcelable {
 
   public static final String ID_NOT_SET = "";
@@ -18,4 +21,26 @@ public abstract class BaseViewModel implements Parcelable {
     public abstract T setModelType(int value);
   }
 
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+
+    if (!(obj instanceof BaseViewModel)) {
+      return false;
+    }
+
+    return equals(id(), ((BaseViewModel) obj).id()) && equals(modelType(), ((BaseViewModel) obj)
+        .modelType());
+  }
+
+  public static boolean equals(Object a, Object b) {
+    return (a == b) || (a != null && a.equals(b));
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(new Object[]{id(), modelType()});
+  }
 }
