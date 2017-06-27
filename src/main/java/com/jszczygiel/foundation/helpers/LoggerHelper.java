1d4fb40a3238514682d83b6320afdf545a8b2e83
Jakub Szczygiel <jszczygiel@wroclawstudio.com>
2017-06-27 14:09:53 +0200

    Changed how log level is set

--- src/main/java/com/jszczygiel/foundation/helpers/LoggerHelper.java
+++ src/main/java/com/jszczygiel/foundation/helpers/LoggerHelper.java
@@ -4,26 +4,23 @@ import android.util.Log;
 
 public class LoggerHelper {
 
-  private static int LOGLEVEL = Log.ASSERT;
-  private static boolean ERROR = LOGLEVEL > Log.ERROR;
-  private static boolean INFO = LOGLEVEL > Log.INFO;
-  private static boolean DEBUG = LOGLEVEL > Log.DEBUG;
+  private static int LOGLEVEL = Log.VERBOSE;
   private static final String TAG = "APP";
 
   public static void logInfo(String message) {
-    if (INFO) {
+    if (LOGLEVEL > Log.INFO) {
       Log.i(TAG, message);
     }
   }
 
   public static void logDebug(String message) {
-    if (DEBUG) {
+    if (LOGLEVEL > Log.DEBUG) {
       Log.d(TAG, message);
     }
   }
 
   public static void logError(String message) {
-    if (ERROR) {
+    if (LOGLEVEL > Log.ERROR) {
       Log.e(TAG, message);
     }
   }
@@ -33,8 +30,12 @@ public class LoggerHelper {
   }
 
   public static void log(Throwable e) {
-    if (INFO) {
+    if (LOGLEVEL > Log.DEBUG) {
       e.printStackTrace();
     }
+  }
+
+  public static void setLogLevel(int logLevel) {
+    LOGLEVEL = logLevel;
   }
 }
